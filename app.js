ce83051549721388881b4cca5ca619a6e3390246
muhammad45401 <saadjani3333@gmail.com>
2024-03-31 16:55:31 +0500

    class room update

--- app.js
+++ app.js
@@ -3,42 +3,40 @@ var bg1 =document.getElementById("bgColor");
 var pls = document.querySelectorAll("#plus")
 
 
-function bg(e){
-    bg1.style.backgroundColor="rgba(153, 153, 153, 0.308)"
-  
-}
 
-function bgm(e){
-    bg1.style.backgroundColor="pink"
-  
-}
 
 
 // var main = document.getElementById("main")
 function showmain(e){
   var main = document.getElementById("main")
+  
+  
  main.setAttribute('class','class-input')
  
 
- main.innerHTML =`<h3 class="text">Create class</h3>
+main .innerHTML =`<h3 class="text">Create class</h3>
  <div class="input1"> <input type="text" class="inp1" id='class' placeholder="class name "></div>
  <div class="input2"> <input type="text" class="inp1" id='selection' placeholder="selection"></div>
   
  
- <button class="btn1"   onclick="deletebtn(this) id='delet'">cancel</button>
-  <button  class="btn2" onclick='card(this)'>creat</button>
+ <button class="btn1"   onclick="deletebtn(this)">cancel</button>
+<button  class="btn2" onclick='card(this)'>creat</button>
 `
 };
 function card(e){
+  
    var card = document.getElementById("card")
-    card.innerHTML =`<div class="card"> <div class="title" id='show'></div>
+   var li = document.createElement('li')
+   card.appendChild(li)
+   
+   li.setAttribute("class","card")
+    li.innerHTML =` <div class="title" id='show'></div>
     <div class="selection" id='show1'> </div>
     
-    <div class="footer"></div> </div>`
+    <div class="footer"></div>`
 
     var userText = document.querySelector('#class').value
    show.append(userText);
-
    var userSlc = document.querySelector("#selection").value
       show1.append(userSlc)
     
@@ -46,6 +44,6 @@ function card(e){
 
 
 function deletebtn(e){
-  Event.target.parentNode.remove()
+  event.target.parentNode.remove()
 
 }  
